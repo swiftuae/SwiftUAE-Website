cc6a8f1b629bad11a36861b1662cf5dd6fc601e5
Ethan Chew <chew_ming_hong_ethan@s2019.ssts.edu.sg>
2023-03-23 08:32:42 +0800

    Deploy

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import './index.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{display: "flex", height: "100vh", alignItems: "center", justifyContent: "center"}}>
+      <img className="logo" alt="Swift UAE Logo" src={require("./assets/logo.png")}></img>
     </div>
   );
 }
